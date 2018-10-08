4ac1d2991d27148bbd139bb2e4828985db29f50b
whatever <matt@svv.im>
2018-10-08 15:37:05 -0400

    :fish_cake: stubbing method that we'll need to debug more complicated scenarios

---
+++ dht/util.go
@@ -0,0 +1,6 @@
+package dht
+
+// FigureOutTopology contains a method for figuring out the topology of the network
+// XXX: Figure out the best way to do something like this
+func FigureOutTopology() {
+}
